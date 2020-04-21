3e9bd06c518c50ab914aa4172a27f5cc6d425337
Dustin Venable <Venny44@yahoo.com>
2020-04-21 06:09:06 -0500

    Add favicon - fix buttons - update bio

--- action.js
+++
@@ -1,7 +0,0 @@
-$(".buttons").on("click", ".button-site", function() {
-    window.open("https://dusven44.github.io/Quiz-App/");
-})
-
-$(".buttons").on("click", ".button-code", function() {
-    window.open("https://github.com/DusVen44/Quiz-App.git");
-})
